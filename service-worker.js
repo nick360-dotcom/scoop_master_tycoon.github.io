b08b0a486c3fe1c4239627c5c27a0656812c8a3a
nick360-dotcom <nicholaslingjiahao@gmail.com>
2026-07-28 00:37:32 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE = 'scoop-master-phone-v4';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles-phone-v4.css?v=4',
+  './game-phone-v4.js?v=4',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
+  );
+});
